e47a7a9479c5602a4c5fb997c3ff02464b237b4b
Justin Asfour <6599551+justinasfour04@users.noreply.github.com>
2023-11-05 17:01:39 -0500

    Cleaned up background and such

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,73 +21,69 @@ export default async function Page() {
         data-x_margin="18"
         data-y_margin="18"
       ></script>
-      <div className="h-full w-full">
-        <div className="absolute h-full w-full bg-gradient-to-b from-[#181C1D00] from-30% via-[#252627] via-80% to-[#252627] to-100% sm:h-[120rem]" />
-        <Background className="absolute w-full md:h-1/2 lg:h-3/4" />
-        <div className="absolute h-full w-full bg-gradient-to-b from-[#181C1D00] from-30% via-[#252627] via-80% to-[#252627] to-100% sm:h-[120rem]" />
-        <div className="bg-gradient-to-b from-[#353535] to-[#090c0f]">
-          <div className="mb-[10rem] flex w-full flex-col items-center">
-            <div className="mt-[5rem] h-[120rem] w-full lg:h-[110rem]">
-              <Arcade />
-            </div>
-            <Section
-              title="WHO DONE IT?"
-              imageClassName="relative h-[25rem] w-[25rem] object-cover"
-              altSrcTuples={[
-                ['Potn pfp', '/img/landing/potn.png'],
-                ['Bd pfp', '/img/landing/birddog.png'],
-              ]}
-              isReverse={false}
-            >
-              <SectionParagraph>
-                Typical Arcade was started by{' '}
-                <Link href="https://twitter.com/powerofthenut">
-                  <span className="underline">@powerofthenut</span>
-                </Link>{' '}
-                and{' '}
-                <Link href="https://twitter.com/birddog9000">
-                  <span className="underline">@birddog9000</span>
-                </Link>{' '}
-                out of the idea of creating games for the Typical Friends
-                ecosystem. We love the community and want to be typical
-                community members who contribute and take part in its growth.
-              </SectionParagraph>
-            </Section>
-            <Section
-              title="WEN GAMES?"
-              imageClassName="relative h-[25rem] w-[25rem] object-cover"
-              altSrcTuples={[['wen pfp', '/img/wen.gif']]}
-              isReverse={true}
-            >
-              <SectionParagraph>
-                Soon.... There is only two of us right now so games will come as
-                soon as we get to them. Stay up to date with announcements by
-                following{' '}
-                <Link href="https://twitter.com/typicalarcade">
-                  <span className="underline">@typicalarcade</span>
-                </Link>{' '}
-                on twitter
-              </SectionParagraph>
-            </Section>
-            <Section
-              title="TYPICAL FRIENDS"
-              imageClassName="relative h-[25rem] w-[25rem] object-cover"
-              altSrcTuples={[['Tf pfp', '/img/landing/tf.png']]}
-              isReverse={false}
-            >
-              <SectionParagraph>
-                <Link href="https://twitter.com/_typicalfriends">
-                  <span className="underline">@_typicalfriends</span>
-                </Link>{' '}
-                is a-typical web3 studio creating top-tier generative art &amp;
-                digital collectibles. Founded by @motionmarkus, we take
-                inspiration from his collections to bring you your daily dose of
-                mini-games.
-              </SectionParagraph>
-            </Section>
+      <Background className="absolute w-full md:h-1/2 lg:h-3/4" />
+      <div className="bg-gradient-to-b from-[#353535] from-0% via-[#252627] via-50% to-[#090c0f] to-100%">
+        <div className="mb-[10rem] flex w-full flex-col items-center">
+          <div className="mt-[5rem] h-[100rem] w-full">
+            <Arcade />
           </div>
-          <Footer />
+          <Section
+            title="WHO DONE IT?"
+            imageClassName="relative h-[25rem] w-[25rem] object-cover"
+            altSrcTuples={[
+              ['Potn pfp', '/img/landing/potn.png'],
+              ['Bd pfp', '/img/landing/birddog.png'],
+            ]}
+            isReverse={false}
+          >
+            <SectionParagraph>
+              Typical Arcade was started by{' '}
+              <Link href="https://twitter.com/powerofthenut">
+                <span className="underline">@powerofthenut</span>
+              </Link>{' '}
+              and{' '}
+              <Link href="https://twitter.com/birddog9000">
+                <span className="underline">@birddog9000</span>
+              </Link>{' '}
+              out of the idea of creating games for the Typical Friends
+              ecosystem. We love the community and want to be typical community
+              members who contribute and take part in its growth.
+            </SectionParagraph>
+          </Section>
+          <Section
+            title="WEN GAMES?"
+            imageClassName="relative h-[25rem] w-[25rem] object-cover"
+            altSrcTuples={[['wen pfp', '/img/wen.gif']]}
+            isReverse={true}
+          >
+            <SectionParagraph>
+              Soon.... There is only two of us right now so games will come as
+              soon as we get to them. Stay up to date with announcements by
+              following{' '}
+              <Link href="https://twitter.com/typicalarcade">
+                <span className="underline">@typicalarcade</span>
+              </Link>{' '}
+              on twitter
+            </SectionParagraph>
+          </Section>
+          <Section
+            title="TYPICAL FRIENDS"
+            imageClassName="relative h-[25rem] w-[25rem] object-cover"
+            altSrcTuples={[['Tf pfp', '/img/landing/tf.png']]}
+            isReverse={false}
+          >
+            <SectionParagraph>
+              <Link href="https://twitter.com/_typicalfriends">
+                <span className="underline">@_typicalfriends</span>
+              </Link>{' '}
+              is a-typical web3 studio creating top-tier generative art &amp;
+              digital collectibles. Founded by @motionmarkus, we take
+              inspiration from his collections to bring you your daily dose of
+              mini-games.
+            </SectionParagraph>
+          </Section>
         </div>
+        <Footer />
       </div>
     </>
   );
--- src/components/landing/background.tsx
+++ src/components/landing/background.tsx
@@ -1,35 +1,19 @@
 import Image from 'next/image';
 import { JSX } from 'react';
 
-import building from '../../../public/img/buildings.png';
-// import cloud from '../../../public/img/cloud.png';
+import background from '../../../public/img/buildings.png';
+import clouds from '../../../public/img/clouds.png';
 import { ClassnameProps } from '../types';
-
-// function clouds(length: number) {
-//   return Array.from({ length }, (_, i) => (
-//     <Image
-//       key={`cloud-${i * 10}`}
-//       className="w-[10rem] lg:w-[15rem]"
-//       src={cloud}
-//       alt="cloud"
-//     />
-//   ));
-// }
 
 export default function Background({ className }: ClassnameProps): JSX.Element {
   return (
     <div className={className}>
-      <div className="flex h-full w-full flex-col items-center gap-[15rem] pt-[5rem] lg:gap-[10rem]">
-        {/* <div className="relative top-[5rem] h-[50rem] w-full md:inset-x-3 md:top-3">
-          <div className="relative -top-[2rem] grid w-full grid-cols-8 md:gap-x-[15rem]">
-            {clouds(8)}
-          </div>
-          <div className="relative left-[5rem] top-[7rem] grid grid-cols-6 md:left-[12rem] w-full">
-            {clouds(6)}
-          </div>
-        </div> */}
-        <div className="h-full w-full">
-          <Image className="h-full w-full" src={building} alt="building" />
+      <div className="pt-[3rem]">
+        <div className="absolute">
+          <Image src={background} alt="background" />
+        </div>
+        <div className="absolute">
+          <Image src={clouds} alt="clouds" />
         </div>
       </div>
     </div>
